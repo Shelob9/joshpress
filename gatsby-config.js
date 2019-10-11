f4547ecbe26ee49dbc2e17ad35e521cff1d7c42e
Josh Pollock <josh@saturdaydrive.io>
2019-10-11 16:48:13 -0400

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,17 +8,46 @@ module.exports = {
     {
       resolve: 'gatsby-source-wordpress',
       options: {
-        // The base url to your WP site.
-        baseUrl: 'wpdemo.gatsbycentral.com',
-        // WP.com sites set to true, WP.org set to false
-        hostingWPCOM: false,
+        baseUrl: "joshpress.net",
         // The protocol. This can be http or https.
-        protocol: 'https',
-        // Use 'Advanced Custom Fields' Wordpress plugin
+        protocol: "https",
+        hostingWPCOM: false,
         useACF: false,
-        auth: {},
-        // Set to true to debug endpoints on 'gatsby build'
-        verboseOutput: false,
+        // Set how many pages are retrieved per API request.
+        perPage: 25,
+        // Search and Replace Urls across WordPress content.
+        //searchAndReplaceContentUrls: {},
+        // Set how many simultaneous requests are sent at once.
+        concurrentRequests: 10,
+        // Set WP REST API routes whitelists
+        // and blacklists using glob patterns.
+        // Defaults to whitelist the routes shown
+        // in the example below.
+        // See: https://github.com/isaacs/minimatch
+        // Example:  `["/*/*/comments", "/yoast/**"]`
+        // ` will either include or exclude routes ending in `comments` and
+        // all routes that begin with `yoast` from fetch.
+        // Whitelisted routes using glob patterns
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/users",
+        ],
+        // Blacklisted routes using glob patterns
+       // excludedRoutes: ["**/posts/1456"],
+        // Set this to keep media sizes.
+        // This option is particularly useful in case you need access to
+        // URLs for thumbnails, or any other media detail.
+        // Defaults to false
+        keepMediaSizes: true,
+        // use a custom normalizer which is applied after the built-in ones.
+        normalizer: function({ entities }) {
+          return entities
+        },
       },
     },
     'gatsby-plugin-sharp',
